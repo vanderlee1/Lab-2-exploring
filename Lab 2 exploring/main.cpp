8ff37a57e112e9bea1a52483ba8250031c601a9e
vanderlee1 <69871267+vanderlee1@users.noreply.github.com>
2020-09-01 20:04:59 -0500

    Add project files.

---
+++ Lab 2 exploring/main.cpp
@@ -0,0 +1,78 @@
+
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void printclass1();//prototype
+void printclass2();
+void printclass3();
+void printt1();
+void printt2();
+void printw3();
+void printth1();
+
+
+const string class1 = "    8:00  Macroeconomics";
+const string class2 = "    11:00 Calculus II";
+const string class3 = "    1:30  C++";
+const string t1= "    9:00  Engineering problems";
+const string t2 = "    1:00  C++";
+const string w3 = "    2:00  Classical Physics";
+const string th1 = "    2:00  Classical Physics";
+
+ 
+
+
+
+int main()
+{
+	cout << "Monday ";
+	printclass1();
+	cout << "\n";
+    cout << "Monday ";
+	printclass2();
+	cout << "\n";
+	cout<< "Monday ";
+	printclass3();
+	cout << "\n";
+	cout << "Tuesday ";
+	printt1();
+
+	return 0;
+
+}
+
+void printclass1()
+{
+	cout << class1;
+
+}
+
+void printclass2()
+{
+	cout << class2;
+}
+
+void printclass3()
+{
+	cout << class3;
+}
+
+void printt1()
+{
+	cout <<t1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
